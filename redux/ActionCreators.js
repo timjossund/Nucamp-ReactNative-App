cf8cb697a229909f4ed4e3b698d7134840def027
timjossund <timjossund@gmail.com>
2021-10-12 20:48:06 -0500

    SwipeGesture

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -175,3 +175,8 @@ export const addFavorite = campsiteId => ({
     payload: campsiteId
 });
 
+export const deleteFavorite = campsiteId => ({
+    type: ActionTypes.DELETE_FAVORITE,
+    payload: campsiteId
+});
+
