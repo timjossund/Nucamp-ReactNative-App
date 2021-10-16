1ad282b81717d175079b3f9333dd52245beeb385
timjossund <timjossund@gmail.com>
2021-10-16 11:05:13 -0500

    bugfix

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -14,7 +14,6 @@ class Reservation extends Component {
             hikeIn: false,
             date: new Date(),
             showCalendar: false,
-            showModal: false
         };
     }
 
@@ -22,14 +21,14 @@ class Reservation extends Component {
         title: 'Reserve Campsite'
     }
 
-    toggleModal() {
-        this.setState({showModal: !this.state.showModal});
-    }
+    // toggleModal() {
+    //     this.setState({showModal: !this.state.showModal});
+    // }
 
-    handleReservation() {
-        //console.log(JSON.stringify(this.state));
-        //this.toggleModal();
-    }
+    // handleReservation() {
+    //     //console.log(JSON.stringify(this.state));
+    //     //this.toggleModal();
+    // }
 
     resetForm() {
         this.setState({
@@ -140,22 +139,6 @@ const styles = StyleSheet.create({
     formItem: {
         flex: 1
     },
-    modal: { 
-        justifyContent: 'center',
-        margin: 20
-    },
-    modalTitle: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        backgroundColor: '#5637DD',
-        textAlign: 'center',
-        color: '#fff',
-        marginBottom: 20
-    },
-    modalText: {
-        fontSize: 18,
-        margin: 10
-    }
 });
 
 export default Reservation;
